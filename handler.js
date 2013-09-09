557ff21dd0d854e4723840b5830d3cd4fb59dd90
rogerano <rogerano@gmail.com>
2013-09-09 12:08:54 +1000

    test client content is optional

--- handler.js
+++ handler.js
@@ -1,17 +1,26 @@
 // Command line options.
 var cmd = require("node-getopt").create([
   ["p", "port=", "Port on which to listen for data requests"],
+  ["t", "test", "Serve static content from testclient directory"],
   ["h", "help", "Display this help message"]
 ]).bindHelp().parseSystem();
 
 // Http request handler.
 var
   httpHandler = require("./lib/httpRequestHandler");
-  server      = require("http").createServer(httpHandler);
+  server      = require("http").createServer(httpHandler),
+  express     = require("express");
 
 // Web socket handler.
 var
   wsHandler   = require("./lib/wsRequestHandler").listen(server);
+
+console.log(cmd.options);
+// Test client setup.
+if (cmd.options.test)
+{
+  httpHandler.use(express.static("./testclient/", { maxAge: 1 }));
+}
 
 // Start the data request handler on designated or default port.
 server.listen(cmd.options.port || 3000);
