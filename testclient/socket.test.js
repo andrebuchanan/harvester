bdd14e610cdd1b71220e03824d47a29132026d49
rogerano <rogerano@gmail.com>
2013-11-15 15:10:49 +1100

    merge test

--- testclient/socket.test.js
+++ testclient/socket.test.js
@@ -6,6 +6,7 @@ var fids = io.connect("http://localhost:3000/fids?pass=123", {
 fids.on("connect", function()
 {
   fids.emit("fleet", { "fleet": "QF" });
+  fids.emit("fleet", { "fleet": "QF" });
 });
 
 fids.on("fids", function(data)
