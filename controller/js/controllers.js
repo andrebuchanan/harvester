be605554491e525d62c67751f9a5c6c75cb8c54c
rogerano <rogerano@gmail.com>
2014-03-05 15:19:53 +1100

    firebase url interface and manipulation

--- controller/js/controllers.js
+++ controller/js/controllers.js
@@ -14,18 +14,62 @@ angular.module('hcontroller.controllers', ["firebase"]).
   })
   //
   // Url controller.
-  .controller("urlCtrl", function($scope, $firebase, $modal, $location, $routeParams)
+  .controller("urlCtrl", function($scope, $firebase, $modal, $location, $routeParams, $firebaseSimpleLogin)
   {
     var that = this;
+    this.urlSaveError = {
+      alert: false
+    };
+    this.urlSearch = {
+      $: '',
+      deleted: false
+    };
+    this.edit = {};
 
     // Create a firebase reference.
     var ref = new Firebase("https://cti-harvester-urls.firebaseio.com/");
+    // Authentication.
+    this.auth = $firebaseSimpleLogin(ref);
+    // Get urls.
     this.urls = $firebase(ref);
+
+    this.authenticate = function(username, password)
+    {
+      this.auth.$login("password", {
+        email: username,
+        password: password
+      }).then(function(user)
+      {
+        that.auth.error = false;
+      },
+      function(error)
+      {
+        that.auth.error = "Invalid email or password";
+      });
+    };
+
+    this.addNewUrl = function(urlId)
+    {
+      // Check that url id isn't already used.
+      if (!this.urls[urlId])
+      {
+        this.urls[urlId] = { id: urlId, enabled: false, name: "New Url", deleted: false };
+      }
+      this.urls.$save(urlId);
+    };
+
+    this.loadUrlData = function(urlId)
+    {
+      urlId = urlId || $routeParams.urlId || false;
+      if (!urlId) return;
+      var uRef = new Firebase("https://cti-harvester-urls.firebaseio.com/" + urlId);
+      this.url = $firebase(uRef);
+      this.edit.unsaved = false;
+    };
 
     if ($routeParams.urlId)
     {
-      var uRef = new Firebase("https://cti-harvester-urls.firebaseio.com/" + $routeParams.urlId);
-      this.url = $firebase(uRef);
+      this.loadUrlData($routeParams.urlId);
     }
 
     // Redirect to url view.
@@ -35,32 +79,22 @@ angular.module('hcontroller.controllers', ["firebase"]).
     };
 
     // Flip the enabled flag on a url.
-    this.updateUrl = function(url)
+    this.toggleState = function(url)
     {
-      this.url.enabled = !this.url.enabled;
+      url.enabled = !url.enabled;
       this.urls.$save();
     };
 
     // Save a url.
     this.saveUrl = function(url)
     {
-      this.urls.$save();
-    };
-
-    //
-    // Open a dialog for display of url information.
-    this.urlInfo = function(url)
-    {
-      $modal.open({
-        templateUrl: "partials/url-info-dialog.html",
-        resolve: {
-          url: function()
-          {
-            return $scope.urlsCtrl.url;
-          }
-        },
-        controller: "modalInstanceCtrl as modal"
+      this.edit.saving = true;
+      var test = url.$save();
+      test.then(function()
+      {
+        that.edit.saving = false;
       });
+      this.edit.unsaved = false;
     };
 
     this.addNewQs = function(key, value)
@@ -72,21 +106,73 @@ angular.module('hcontroller.controllers', ["firebase"]).
       key = ""; value = "";
     };
 
-  })
-  //
-  // Controller for modal
-  .controller("modalInstanceCtrl", function($scope, $modalInstance, url)
-  {
-    this.url = url;
-    this.close = function()
+    this.removeQs = function(qsKey)
     {
-      $modalInstance.close();
+      delete this.url.qs[qsKey];
     };
 
-    this.saveUrl = function()
+    this.updateQs = function(qsKey, qsValue)
     {
-      console.log(url);
-      this.close();
+      this.url.qs[qsKey] = qsValue;
+    };
+
+    this.addNewDt = function(newDt)
+    {
+      this.url.dataTypes.push(newDt);
+    };
+
+    this.removeDt = function(dataType)
+    {
+      var remIdx = this.url.dataTypes.indexOf(dataType);
+      if (remIdx >= 0) this.url.dataTypes.splice(remIdx, 1);
+    };
+
+    this.unsaved = function()
+    {
+      this.edit.unsaved = true;
+    };
+
+    this.removeField = function(fieldName)
+    {
+      delete this.url[fieldName];
+    };
+
+    this.deleteUrl = function(url)
+    {
+      var modalInst = $modal.open({
+        templateUrl: "partials/delete-url-dialog.html",
+        controller: "DeleteUrlCtrl as deleteUrlCtrl",
+        resolve: {
+          url: function() { return url; }
+        }
+      });
+
+      // When the dialog is closed by clicking "Confirm"...
+      modalInst.result.then(function(urlToDelete)
+      {
+        urlToDelete.deleted = true;
+        urlToDelete.enabled = false;
+        urlToDelete.$save();
+        $location.path("/");
+      });
+    };
+
+  })
+  .controller("DeleteUrlCtrl", function($modalInstance,  url)
+  {
+    this.url = url;
+    this.cancel = function()
+    {
+      $modalInstance.dismiss('cancel');
+    };
+
+    this.deleteUrl = function()
+    {
+      // Test input name against url name.
+      if (this.urlName === this.url.name)
+      {
+        $modalInstance.close(this.url);
+      }
     };
   });
 })();
